846f922d8fcb12b2f49a4caf268d7437123844e6
oahray <rodneyoare@gmail.com>
2017-09-19 22:51:23 +0100

    chore (gulp): convert from grunt to gulp - create gulpfile with equivalent tasks - modify npm scripts for start, test and postinstall [Finishes #151136242]

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const nodemon = require('gulp-nodemon');
+const sass = require('gulp-sass');
+const bower = require('gulp-bower');
+const browserSync = require('browser-sync');
+
+gulp.task('watch', () => {
+  gulp.watch('app/views/**', browserSync.reload());
+  gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload());
+  gulp.watch('public/views/**', browserSync.reload());
+  // gulp.watch(['public/css/common.scss, public/css/views/articles.scss'], ['sass']);
+  gulp.watch('public/css/**', ['sass']);
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['gruntfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js'])
+  .pipe(eslint());
+});
+
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    watch: ['app', 'config'],
+    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+    env: {
+      PORT: 3000
+    }
+  });
+});
+
+gulp.task('concurrent', ['nodemon', 'watch']);
+
+gulp.task('mochaTest', () => {
+  gulp.src(['test/**/*.js'])
+  .pipe(mocha(
+    // {
+    // reporter: 'spec'
+    // }
+  ));
+});
+
+gulp.task('sass', () => {
+  gulp.src('public/css/common.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('public/common.css'));
+});
+
+gulp.task('bower', () => {
+  bower().pipe(gulp.dest('./public/lib'));
+});
+
+gulp.task('install', ['bower']);
+
+gulp.task('test', ['mochaTest']);
+
+gulp.task('default', ['lint', 'concurrent', 'sass']);
+
