49b71c7e30c48948ce55815ee839bb39613be9aa
Benny Ogidan <bennyogidan@Andelas-MacBook-Pro.local>
2017-09-20 17:52:25 +0100

    #####Sign up  and Sign in routes working on Node js and tested with Postmaster
    #####Validation checks constructed for each route

--- app/controllers/middleware/auth.js
+++ app/controllers/middleware/auth.js
@@ -1,18 +1,18 @@
-jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-class UserAuthenticate {
-	/**
+// class UserAuthenticate {
+/**
 * this fetched the token and places it in the x-access-token header
 * @private
 * @param {object} req - request nobject
 * @return {string} token response
 */
-	static getToken(req) {
-		const token = req.body.token || req.headers['x-access-token'] || req.headers.Authorization;
-		return token;
-	}
+const getToken = (req) => {
+  const token = req.body.token || req.headers['x-access-token'] || req.headers.Authorization;
+  return token;
+};
 
-	/**
+/**
 * authenticates a the json web token  to be appended in routes that need to be authenticated
 * @public
 * @param {object} req - request object
@@ -20,42 +20,44 @@ class UserAuthenticate {
 * @param {function} next - next function to be called on the pobject
 * @return {undefined} if not defined send a response to the server indicating this
 */
-	static authenticate(req, res, next) {
-		const token = getToken(req);
-		if (token) {
-			jwt.verify(token, process.env.SECRET, (error, decoded) => {
-				if (error) {
-					return res.status(401).json({ message: 'Unauthorised access' });
-				}
-				req.user = decoded;
-				next();
-			});
-		} else {
-			res.status(403).send({
-				success: false,
-				message: 'Incorrect, Please try Login again'
-			});
-		}
-	}
+const authenticate = (req, res, next) => {
+  const token = getToken(req);
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ message: 'Unauthorised access' });
+      }
+      req.user = decoded;
+      next();
+    });
+  } else {
+    res.status(403).send({
+      success: false,
+      message: 'Incorrect, Please try Login again'
+    });
+  }
+};
 
-	/** 
+/** 
  * Signs a json web token with the supplied parameters
  * @param  {String}  - id
  * @param  {String}  - email
  * @param  {String}  - username
  */
-	static getJWT(id, email, username) {
-		jwt.sign(
-			{
-				id,
-				email,
-				username
-			},
-			'Secret',
-			{
-				expiresIn: 1440
-			}
-		);
-	}
-}
-export default UserAuthenticate;
+const getJWT = (id, email, username) => {
+  jwt.sign(
+    {
+      id,
+      email,
+      username
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: 1440
+    }
+  );
+};
+// }
+// export default UserAuthenticate;
+
+module.exports = { getJWT, authenticate, getToken };
