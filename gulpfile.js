273b865ce6e0a0da802021100291a67934f826f4
oahray <rodneyoare@gmail.com>
2017-09-20 14:01:18 +0100

    chore(maintain): modify eslint piping

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const nodemon = require('gulp-nodemon');
+const sass = require('gulp-sass');
+const bower = require('gulp-bower');
+const browserSync = require('browser-sync');
+
+gulp.task('watch', () => {
+  gulp.watch('app/views/**', browserSync.reload());
+  gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload());
+  gulp.watch('public/views/**', browserSync.reload());
+  gulp.watch('public/css/**/*scss', ['sass']);
+  gulp.watch('public/css/**', browserSync.reload());
+});
+
+gulp.task('lint', () => {
+  gulp.src(['gulpfile.js', 'public/js/**/*.js', 'app/**/*.js', 'test/**/*.js'])
+    .pipe(eslint({ config: '.eslintrc' }))
+});
+
+gulp.task('nodemon', () => {
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    watch: ['app', 'config'],
+    ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+    env: {
+      PORT: 3000
+    }
+  });
+});
+
+gulp.task('concurrent', ['nodemon', 'watch']);
+
+gulp.task('mochaTest', () => {
+  gulp.src(['test/**/*.js'])
+    .pipe(mocha({ reporter: 'spec' }
+    ));
+});
+
+gulp.task('sass', () => {
+  gulp.src('public/css/common.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('bower', () => {
+  bower().pipe(gulp.dest('./bower_components'))
+});
+
+gulp.task('install', ['bower']);
+
+gulp.task('test', ['mochaTest']);
+
+gulp.task('default', ['lint', 'concurrent', 'sass']);
