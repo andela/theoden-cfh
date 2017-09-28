95380df07383d3155cc78bb6b8392172679063cc
Benny Ogidan <bennyogidan@Andelas-MacBook-Pro.local>
2017-09-28 17:17:07 +0100

    #### More refractoring code

--- config/express.js
+++ config/express.js
@@ -63,7 +63,7 @@ module.exports = function (app, passport, mongoose) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    //app.use('/api', auth);
+    app.use('/api', auth);
 
     //routes should be at the last
     app.use(app.router);
