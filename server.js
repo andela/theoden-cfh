baf228b3239458666e321758f582f89d4c7ed1e9
Raphael Etim <andeladeveloper@Andelas-MacBook-Pro-3.local>
2017-09-29 14:05:37 +0100

    Fix(bug fix): Fixed Errors in file

--- server.js
+++ server.js
@@ -2,68 +2,59 @@
 * Module dependencies.
 */
 require('dotenv').config();
-const express = require('express'),
-  fs = require('fs'),
-  passport = require('passport'),
-  logger = require('mean-logger'),
-  io = require('socket.io');
+const express = require('express');
+const fs = require('fs');
+const passport = require('passport');
+const logger = require('mean-logger');
+const io = require('socket.io');
+// Load configurations
+// if test env, load example file
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';// eslint-disable-line no-multi-assign no-unused-vars
+const config = require('./config/config');
+const auth = require('./config/middlewares/authorization');
+const mongoose = require('mongoose');
 /**
 * Main application entry file.
 * Please note that the order of loading is important.
 */
-
-// Load configurations if test env, load example file
-const env = process.env.NODE_ENV || 'development',
-  config = require('./config/config'),
-  auth = require('./config/middlewares/authorization'),
-  mongoose = require('mongoose');
-
 // Bootstrap db connection
-const db = mongoose.connect(config.db);
-
+mongoose.connect(config.db);
+// compartible with mongoose 4.11
 // Bootstrap models
-const modelsPath = __dirname + '/app/models';
-let walk =  (path) => {
-  fs
-    .readdirSync(path)
-    .forEach( (file) => {
-      let newPath = path + '/' + file;
-      let stat = fs.statSync(newPath);
-      if (stat.isFile()) {
-        if (/(.*)\.(js|coffee)/.test(file)) {
-          require(newPath);
-        }
-      } else if (stat.isDirectory()) {
-        walk(newPath);
+const modelsPath = `${__dirname}/app/models`;
+const walk = (path) => {
+  fs.readdirSync(path).forEach((file) => {
+    const newPath = `${path}/${file}`;
+    const stat = fs.statSync(newPath);
+    if (stat.isFile()) {
+      if (/(.*)\.(js|coffee)/.test(file)) {
+        require(newPath); // eslint-disable-line global-require import/no-dynamic-require
       }
-    });
+    } else if (stat.isDirectory()) {
+      walk(newPath);
+    }
+  });
 };
 walk(modelsPath);
-
 // bootstrap passport config
 require('./config/passport')(passport);
-
-let app = express();
-
+const app = express();
 app.use((req, res, next) => {
   next();
 });
-
 // express settings
 require('./config/express')(app, passport, mongoose);
-
 // Bootstrap routes
 require('./config/routes')(app, passport, auth);
-
 // Start the app by listening on <port>
-let port = config.port;
-let server = app.listen(port);
-let ioObj = io.listen(server, {log: false});
+const port = config.port;
+const server = app.listen(port);
+const ioObj = io.listen(server, { log: false });
 // game logic handled here
 require('./config/socket/socket')(ioObj);
-
+console.log(`Express app started on port ${port}`);
 // Initializing logger
 logger.init(app, passport, mongoose);
-
 // expose app
- module.exports = app;
+// exports =
+module.exports = app;
