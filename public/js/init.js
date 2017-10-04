2ce28fa6eae0393280f35b66ef74a0c971771fb5
Solomon Nsumei <solnsumei@users.noreply.github.com>
2017-10-04 17:35:08 +0100

    #151136250 UI Redesign for User Authentication Screen (#29)
    * feat(auth-pages-ui): redesign of authentication pages UI
    
    - redesign sign up and sign in pages using material design
    - redesign of choose avatar page using material design
    - fix issue with coveralls badge showing unknown
    
    [Delivers #151136250]

--- public/js/init.js
+++ public/js/init.js
@@ -5,11 +5,10 @@
 // window.init = function() {
 //     window.bootstrap();
 // };
-if (window.location.hash == "#_=_") window.location.hash = "#!";
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 // $(document).ready(function() {
 //     //Fixing facebook bug with redirect
-    
 
-//     //Then init the app
-//     window.init();
-// });
+//    //Then init the app
+//    window.init();
+//    // });
